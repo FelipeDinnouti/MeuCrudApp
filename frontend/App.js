0ce75ac9d5aae0bcf2e81fd6c387a6d1bed717c8
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2026-03-27 12:55:34 -0300

    screens correction?

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import AddEditScreen from './src/screens/AddEditScreen';
+import HomeScreen from './src/screens/HomeScreen.js';
+import AddEditScreen from './src/screens/AddEditScreen.js';
 import { View } from 'react-native-web';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{ headerShown: false }}
-          initialRouteName="HomeScreen"
-        >
-
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="AddEditScreen" component={AddEditScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <View>
-        <Text>"WJKAHSFkjh</Text>
-      </View>
-    </GestureHandlerRootView>
+    <View>
+      <Text>ASDLKJ</Text>
+    </View>
   );
 }
