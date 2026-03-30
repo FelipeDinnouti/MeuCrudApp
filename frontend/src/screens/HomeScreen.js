1c0401f9ea21c619137b581b531b07a70f5e7323
Falkun <66378650+FelipeDinnouti@users.noreply.github.com>
2026-03-30 12:42:11 -0300

    feat: implementando pesquisa de usuarios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import { View, Text, FlatList, Button, StyleSheet, TextInput } from "react-native";
 import { useFocusEffect } from '@react-navigation/native';
 
 import styles from "../styles/styles";
@@ -8,6 +8,7 @@ import { CardPersonal } from "../components/cardPersonal";
 
 export default function HomeScreen({ navigation }){
     const [people, setPeople] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     async function loadPeople() {
         try {
@@ -17,6 +18,25 @@ export default function HomeScreen({ navigation }){
             console.error("Error loading people:", error);
         }
     }
+
+    const filteredPeople = useMemo(() => {
+        if (!searchQuery.trim()) return people; 
+
+        let filtered = []
+
+        for (const person of people) {
+            const name = person.firstName?.toLowerCase() + " " + person.lastName?.toLowerCase() || '';
+
+            console.log()
+
+            if (!name.includes(searchQuery.toLowerCase())) { continue }
+
+            filtered.push(person)
+        }
+
+        return filtered
+
+      }, [people, searchQuery]);
 
     useFocusEffect(
         useCallback(() => {
@@ -33,11 +53,20 @@ export default function HomeScreen({ navigation }){
                 onPress={() => navigation.navigate("AddEditScreen")}
             />
 
+            <View style={ {height: 20} }></View>
+
+            <TextInput
+                    value={searchQuery}
+                    onChangeText={setSearchQuery}
+                    style={styles.input}
+                    placeholder="Pesquise..."
+            />
+
             <FlatList
-                data={people}
+                data={filteredPeople}
                 keyExtractor={(item) => item.id.toString()} // Key used in react's code to keep track of list items
                 renderItem={({ item }) => (
-                    <CardPersonal 
+                    <CardPersonal
                         item={item}
                         navigation={navigation}
                         refresh={loadPeople}
